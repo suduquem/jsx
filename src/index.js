abccf07e45a11e9f9c3d778ff5ecfccdea56b8b5
Susana Duque <su.duquem@gmail.com>
2020-11-17 11:31:49 -0500

    A Form is implemented

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
+// Import the React and ReactDOM
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Create a react component
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function getButtonText() {
+  return 'Click on me!';
+}
+
+const App = () => {
+  // const App = function () {
+  return (
+    <div>
+      <label htmlFor='name'>Enter name:</label>
+      <input id='name' type='text' />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {getButtonText()} 
+      </button>
+    </div>
+  );
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector('#root'));
